278e5dc909fdfde83ddbe2bd5b784cab63a73bb9
Matej Hladiš <matej.hladis@gmail.com>
2020-01-12 11:17:01 +0100

    Initial commit. GAN  according to modhttps://pathmind.com/wiki/generative-adversarial-network-ganel

--- GAN/GAN/GAN.py
+++ GAN/GAN/GAN.py
@@ -1,1 +1,153 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from math import floor
+import os
+from GAN_model import *
 
+
+def show_image(image_matrix):
+    plt.figure()
+    plt.imshow(image_matrix)
+    plt.colorbar()
+    plt.grid(False)
+    plt.show()
+
+def prepro_dataset(images):
+    """
+    """
+    images = (images - np.mean(images)) / np.std(images)
+    # images = images / np.max(images)
+    images = images.astype('float32')
+    return images
+
+
+learning_rate = 0.0002
+batch_size = 52
+epochs = 30000
+original_batch_size = floor(batch_size/2)
+gen_batch_size = floor(batch_size/2)
+noise_shape = 100
+disc_restore = True
+gen_restore = True
+
+mnist = tf.keras.datasets.mnist
+(train_images, _), (test_images, _) = mnist.load_data()
+
+train_images = prepro_dataset(train_images)
+train_dataset = tf.data.Dataset.from_tensor_slices(train_images) 
+
+train_dataset = train_dataset.shuffle(train_images.shape[0])
+train_dataset = train_dataset.batch(original_batch_size)
+train_dataset = train_dataset.prefetch(batch_size * 4)
+
+# Init:
+image_shape = train_images[0].shape
+
+discriminator = Discriminator(image_shape)
+generator = Generator(noise_shape, image_shape)
+
+opt_disc = tf.optimizers.Adam(learning_rate=learning_rate)
+opt_gen = tf.optimizers.Adam(learning_rate=learning_rate)
+
+from datetime import datetime
+start_time = datetime.now().strftime("%Y_%m_%d__%H_%M_%S")
+writer = tf.summary.create_file_writer(os.path.join('tf_logs', start_time))
+disc_ckpt = tf.train.Checkpoint(step=tf.Variable(1), model=discriminator)
+disc_ckpt_manager = tf.train.CheckpointManager(disc_ckpt, os.path.join('tf_logs', 'tf_ckpts_disc'), max_to_keep=3)
+gen_ckpt = tf.train.Checkpoint(step=tf.Variable(1), model=generator)
+gen_ckpt_manager = tf.train.CheckpointManager(gen_ckpt, os.path.join('tf_logs', 'tf_ckpts_gen'), max_to_keep=3)
+
+# Restore Discriminator parameters:
+if disc_restore:
+    disc_ckpt.restore(disc_ckpt_manager.latest_checkpoint)
+    if disc_ckpt_manager.latest_checkpoint:
+        print("Restored from {}".format(disc_ckpt_manager.latest_checkpoint))
+else:
+    print("Initializing Discriminator from scratch.")
+
+# Restore Generator parameters:
+if gen_restore:
+    gen_ckpt.restore(gen_ckpt_manager.latest_checkpoint)
+    if gen_ckpt_manager.latest_checkpoint:
+        print("Restored from {}".format(gen_ckpt_manager.latest_checkpoint))
+else:
+    print("Initializing Discriminator from scratch.")
+
+# Func to train Discriminator
+def training_Discriminator(batch_original_x):
+    input_noise = tf.random.normal(shape = (gen_batch_size, noise_shape),
+                     mean=0.0,
+                     stddev=1.0,
+                     dtype=tf.dtypes.float32,
+                     seed=None,
+                     name='input_noise')
+    batch_gen_x = generator(input_noise)
+
+    train_discriminator(loss = loss_discriminator, 
+                        Discriminator_model = discriminator, 
+                        opt = opt_disc, 
+                        batch_original_x = batch_original_x, 
+                        batch_gen_x = batch_gen_x)
+    loss_disc = loss_discriminator(Discriminator_model = discriminator, 
+                                   batch_original_x = batch_original_x, 
+                                   batch_gen_x = batch_gen_x)
+    return loss_disc
+
+# Func to train Generator
+def training_Generator():
+    noise_batch_x = tf.random.normal(shape = (batch_size, noise_shape),
+                     mean=0.0,
+                     stddev=1.0,
+                     dtype=tf.dtypes.float32,
+                     seed=None,
+                     name='noise_batch_x')
+    
+    train_generator(loss = loss_generator, 
+                    Generator_model = generator, 
+                    opt = opt_gen, 
+                    noise_batch_x = noise_batch_x, 
+                    Discriminator_model = discriminator)
+    loss_gen = loss_generator(Generator_model = generator, 
+                              noise_batch_x = noise_batch_x, 
+                              Discriminator_model = discriminator)
+    return loss_gen
+
+@tf.function
+def training_GAN(batch_original_x):
+    loss_disc = training_Discriminator(batch_original_x)
+    loss_gen = training_Generator()
+    return loss_disc, loss_gen
+
+
+
+with writer.as_default():
+    with tf.summary.record_if(True):
+        for epoch in range(epochs):
+            for step, batch_original_x in enumerate(train_dataset):
+                
+                if step == 0: # Save training graph
+                    tf.summary.trace_on(graph=True, profiler=False)
+                    loss_disc, loss_gen = training_GAN(batch_original_x)
+                    print('exporting model')
+                    tf.summary.trace_export(name="disc_model_trace", step=epoch, profiler_outdir=os.path.join('tf_logs', start_time))
+                else:
+                    loss_disc, loss_gen = training_GAN(batch_original_x)
+                tf.summary.scalar('loss_disc', loss_disc, step=step)
+                tf.summary.scalar('loss_gen', loss_gen, step=step)
+                  
+                if step%50 == 0:
+                    print('epoch:{}   step: {}'.format(epoch, step))
+                    print('loss_disc: {}'.format(loss_disc))
+                    print('loss_gen: {}'.format(loss_gen))
+
+        # Save checkpoint after k epochs:
+        disc_ckpt.step.assign_add(1)
+        gen_ckpt.step.assign_add(1)
+        if epoch % 1 == 0:
+          disc_save_path = disc_ckpt_manager.save()
+          gen_save_path = gen_ckpt_manager.save()
+          print("Saved checkpoint for step {}: {}".format(int(disc_ckpt.step), disc_save_path))
+          print("Saved checkpoint for step {}: {}".format(int(gen_ckpt.step), gen_save_path))
+
+
+# backend.clear_session()
--- GAN/GAN/GAN_model.py
+++ GAN/GAN/GAN_model.py
@@ -1,5 +1,3 @@
-
-
 import tensorflow as tf
 
 class Discriminator(tf.keras.Model):
@@ -7,7 +5,7 @@ class Discriminator(tf.keras.Model):
         super(Discriminator, self).__init__()
 
         self.reshape_to_dense = tf.keras.layers.Reshape(input_shape = img_shape, 
-                                                        target_shape = (100,))
+                                                        target_shape = (tf.math.reduce_prod(img_shape),))
 
         self.dense_1 = tf.keras.layers.Dense(units = 512,
                                               activation = tf.nn.leaky_relu,
@@ -21,15 +19,18 @@ class Discriminator(tf.keras.Model):
                                               activation = tf.nn.sigmoid,
                                               use_bias = True)
 
+    @tf.function
     def call(self, input_features):
         """
         """
+        # with tf.name_scope('disc_model'):
         to_dense = self.reshape_to_dense(input_features)
         d_1 = self.dense_1(to_dense)
-        d_2 = self.dense_1(d_1)
+        d_2 = self.dense_2(d_1)
         d_out = self.dense_output(d_2)
         return d_out
 
+@tf.function
 def loss_discriminator(Discriminator_model, batch_original_x, batch_gen_x):
     """
     loss for the discrimnator.
@@ -38,16 +39,19 @@ def loss_discriminator(Discriminator_model, batch_original_x, batch_gen_x):
     -----------
 
     """
-    loss_original = tf.reduce_mean(tf.square(tf.subtract(Discriminator_model(batch_original_x), tf.ones(batch_original_x.shape))))
-    loss_gen = tf.reduce_mean(tf.square(tf.subtract(Discriminator_model(batch_gen_x), tf.zeros(batch_gen_x.shape))))
+    # with tf.name_scope("disc_loss"):
+    loss_original = tf.reduce_mean(tf.square(tf.subtract(Discriminator_model(batch_original_x), tf.ones(batch_original_x.shape[0]))))
+    loss_gen = tf.reduce_mean(tf.square(tf.subtract(Discriminator_model(batch_gen_x), tf.zeros(batch_gen_x.shape[0]))))
     loss = tf.math.add(loss_original, loss_gen)
     return loss
 
+@tf.function
 def train_discriminator(loss, Discriminator_model, opt, batch_original_x, batch_gen_x):
-  with tf.GradientTape() as tape:
-    gradients = tape.gradient(loss(Discriminator_model, batch_original_x, batch_gen_x), Discriminator_model.trainable_variables)
-    gradient_variables = zip(gradients, Discriminator_model.trainable_variables)
-    opt.apply_gradients(gradient_variables)
+    # with tf.name_scope("disc_train"):
+    with tf.GradientTape() as tape:
+        gradients = tape.gradient(loss(Discriminator_model, batch_original_x, batch_gen_x), Discriminator_model.trainable_variables)
+        gradient_variables = zip(gradients, Discriminator_model.trainable_variables)
+        opt.apply_gradients(gradient_variables)
 
         
 
@@ -58,55 +62,60 @@ class Generator(tf.keras.Model):
     def __init__(self, noise_shape, img_shape):
         super(Generator, self).__init__()
 
-        self.dense_input = tf.keras.layer.Dense(input_shape = noise_shape,
+        self.dense_input = tf.keras.layers.Dense(input_shape = (noise_shape, ),
                                                 units = 256,
                                                 activation = tf.nn.leaky_relu,
                                                 use_bias = True)
 
         self.batch_norm_1 = tf.keras.layers.BatchNormalization(momentum = 0.8)
-        self.dense_1 = tf.keras.layer.Dense(units = 512,
+        self.dense_1 = tf.keras.layers.Dense(units = 512,
                                             activation = tf.nn.leaky_relu,
                                             use_bias = True)
 
         self.batch_norm_2 = tf.keras.layers.BatchNormalization(momentum = 0.8)
-        self.dense_2 = tf.keras.layer.Dense(units = 1024,
+        self.dense_2 = tf.keras.layers.Dense(units = 1024,
                                             activation = tf.nn.leaky_relu,
                                             use_bias = True)
         
         self.batch_norm_3 = tf.keras.layers.BatchNormalization(momentum = 0.8)
-        self.dense_3 = tf.keras.layer.Dense(units = tf.math.reduce_prod(img_shape),
+        self.dense_3 = tf.keras.layers.Dense(units = tf.math.reduce_prod(img_shape),
                                             activation = tf.math.tanh,
                                             use_bias = True)
 
         self.reshape_to_img = tf.keras.layers.Reshape(target_shape = img_shape)
 
+    @tf.function
     def call(self, input_noise):
         """
         """
+        # with tf.name_scope('gen_model'):
         d_in = self.dense_input(input_noise)
         b_1 = self.batch_norm_1(d_in)
         d_1 = self.dense_1(b_1)
         b_2 = self.batch_norm_2(d_1)
         d_2 = self.dense_2(b_2)
-        b_3 = self.batch_norm(d_2)
+        b_3 = self.batch_norm_3(d_2)
         d_3 = self.dense_3(b_3)
         to_img = self.reshape_to_img(d_3)
         return to_img
 
-
-def loss_generator(Generator_model, batch_x, Discriminator_model):
+@tf.function
+def loss_generator(Generator_model, noise_batch_x, Discriminator_model):
     """
-    Generator loss: \sum_{x\sim N(0,1)} (g(f(x)) - 1)^2
+    Generator loss: E_{x\sim N(0,1)} (g(f(x)) - 1)^2
 
     Parameters:
     -----------
 
     """
-    loss = tf.reduce_mean(tf.square(tf.subtract(Discriminator_model(Generator_model(batch_x)), tf.ones(batch_x.shape))))
+    # with tf.name_scope("gen_loss"):
+    loss = tf.reduce_mean(tf.square(tf.subtract(Discriminator_model(Generator_model(noise_batch_x)), tf.ones(noise_batch_x.shape[0]))))
     return loss
 
-def train_generator(loss, Generator_model, opt, batch_x, Discriminator_model):
+@tf.function
+def train_generator(loss, Generator_model, opt, noise_batch_x, Discriminator_model):
+  # with tf.name_scope("gen_train"):
   with tf.GradientTape() as tape:
-    gradients = tape.gradient(loss(Generator_model, batch_x, Discriminator_model), Generator_model.trainable_variables)
-    gradient_variables = zip(gradients, Generator_model.trainable_variables)
-    opt.apply_gradients(gradient_variables)
+      gradients = tape.gradient(loss(Generator_model, noise_batch_x, Discriminator_model), Generator_model.trainable_variables)
+      gradient_variables = zip(gradients, Generator_model.trainable_variables)
+      opt.apply_gradients(gradient_variables)
