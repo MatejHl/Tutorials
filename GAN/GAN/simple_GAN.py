7a2cc56581ce287a51801f355cfd434cf6dbbce6
Matej Hladiš <matej.hladis@gmail.com>
2020-02-05 17:35:23 +0100

    GAN backup.

--- GAN/GAN/simple_GAN.py
+++ GAN/GAN/simple_GAN.py
@@ -3,74 +3,48 @@ import numpy as np
 from math import floor
 import os
 from GAN_model import *
-from datetime import datetime
+
+# pareto_dist = np.random.pareto(a=2, size=10000)
+# pareto_dist = pareto_dist[np.where(pareto_dist < 10)[0]]
+# _ = plt.hist(pareto_dist, bins='auto')
+# plt.show()
 
 
-def show_image(image_matrix):
-    plt.figure()
-    plt.imshow(image_matrix)
-    plt.colorbar()
-    plt.grid(False)
-    plt.show()
 
-def prepro_dataset(images):
-    """
-    """
-    images = (images - np.mean(images)) / np.std(images)
-    # images = images / np.max(images)
-    images = images.astype('float32')
-    return images
-
-
-learning_rate = 0.00005
-batch_size = 208
+learning_rate = 0.00004
+batch_size = 2048
 epochs = 30000
 original_batch_size = floor(batch_size/2)
 gen_batch_size = floor(batch_size/2)
-noise_shape = 100
-eps_disc = 0.01
-eps_gen = 0.00001
-try_disc_mod = 100
-try_gen_mod =  10
+noise_shape = 5
+eps_disc = 0.001
+eps_gen = 0.001
 disc_restore = True
 gen_restore = True
 
-mnist = tf.keras.datasets.mnist
-(train_images, _), (test_images, _) = mnist.load_data()
 
-train_images = prepro_dataset(train_images)
-train_dataset = tf.data.Dataset.from_tensor_slices(train_images) 
+train_pareto = np.random.pareto(a=2, size=(100000,1))
+train_dataset = tf.data.Dataset.from_tensor_slices(train_pareto) 
 
-train_dataset = train_dataset.shuffle(train_images.shape[0])
+train_dataset = train_dataset.shuffle(train_pareto.shape[0])
 train_dataset = train_dataset.batch(original_batch_size)
 train_dataset = train_dataset.prefetch(batch_size * 4)
 
 # Init:
-image_shape = train_images[0].shape
 
-discriminator = Discriminator(image_shape)
-generator = Generator(noise_shape, image_shape)
+discriminator = simple_Discriminator()
+generator = simple_Generator(noise_shape)
 
 opt_disc = tf.optimizers.Adam(learning_rate=learning_rate)
 opt_gen = tf.optimizers.Adam(learning_rate=learning_rate)
 
-if disc_restore or gen_restore:
-    recent_time = datetime.min
-    for filename in os.listdir("tf_logs"):
-        try:
-            tim = datetime.strptime(filename, "%Y_%m_%d__%H_%M_%S")
-        except:
-            continue
-        if tim >= recent_time: 
-            recent_time = tim
-    start_time = recent_time.strftime("%Y_%m_%d__%H_%M_%S")
-else:
-    start_time = datetime.now().strftime("%Y_%m_%d__%H_%M_%S")
-writer = tf.summary.create_file_writer(os.path.join('tf_logs', start_time))
+from datetime import datetime
+start_time = datetime.now().strftime("%Y_%m_%d__%H_%M_%S")
+writer = tf.summary.create_file_writer(os.path.join('paret_test', 'tf_logs', start_time))
 disc_ckpt = tf.train.Checkpoint(step=tf.Variable(1), model=discriminator)
-disc_ckpt_manager = tf.train.CheckpointManager(disc_ckpt, os.path.join('tf_logs', 'tf_ckpts_disc'), max_to_keep=10)
+disc_ckpt_manager = tf.train.CheckpointManager(disc_ckpt, os.path.join('paret_test', 'tf_logs', 'tf_ckpts_disc'), max_to_keep=3)
 gen_ckpt = tf.train.Checkpoint(step=tf.Variable(1), model=generator)
-gen_ckpt_manager = tf.train.CheckpointManager(gen_ckpt, os.path.join('tf_logs', 'tf_ckpts_gen'), max_to_keep=10)
+gen_ckpt_manager = tf.train.CheckpointManager(gen_ckpt, os.path.join('paret_test', 'tf_logs', 'tf_ckpts_gen'), max_to_keep=3)
 
 # Restore Discriminator parameters:
 if disc_restore:
@@ -86,7 +60,7 @@ if gen_restore:
     if gen_ckpt_manager.latest_checkpoint:
         print("Restored from {}".format(gen_ckpt_manager.latest_checkpoint))
 else:
-    print("Initializing Generator from scratch.")
+    print("Initializing Discriminator from scratch.")
 
 # Func to train Discriminator
 @tf.function
@@ -111,13 +85,7 @@ def training_Discriminator(batch_original_x):
 
 # Func to train Generator
 @tf.function
-def training_Generator():
-    noise_batch_x = tf.random.normal(shape = (batch_size, noise_shape),
-                                                     mean=0.0,
-                                                     stddev=1.0,
-                                                     dtype=tf.dtypes.float32,
-                                                     seed=None,
-                                                     name='noise_batch_x')
+def training_Generator(noise_batch_x):    
     train_generator(loss = loss_generator_cross_entropy, 
                     Generator_model = generator, 
                     opt = opt_gen, 
@@ -128,18 +96,17 @@ def training_Generator():
                               Discriminator_model = discriminator)
     return loss_gen
 
-
-mean_loss_disc = 1.0
-mean_loss_gen = 1.0
+mean_loss_disc = 0.0
+mean_loss_gen = 0.0
 with writer.as_default():
     with tf.summary.record_if(True):
         for epoch in range(epochs):
             print('epoch:{}'.format(epoch))
-            if mean_loss_disc >= eps_disc or epoch % try_disc_mod == 0:
+            if mean_loss_disc >= eps_disc or epoch % 40 == 0:
                 mean_loss_disc = 0.0
                 disc_steps_n = 0
                 for disc_step, batch_original_x in enumerate(train_dataset):
-                    
+
                     loss_disc = training_Discriminator(batch_original_x)
                     tf.summary.scalar('loss_disc', loss_disc, step=disc_step)
                     mean_loss_disc += loss_disc
@@ -147,32 +114,44 @@ with writer.as_default():
                 mean_loss_disc = mean_loss_disc/disc_steps_n
                 print('mean_loss_disc: {}'.format(mean_loss_disc))
 
-            if mean_loss_gen >= eps_gen or epoch % try_gen_mod == 0:
+            if mean_loss_gen >= eps_gen or epoch % 40 == 0:
                 mean_loss_gen = 0.0
                 for gen_step in range(disc_steps_n):
-                    loss_gen = training_Generator()
+                    noise_batch_x = tf.random.normal(shape = (batch_size, noise_shape),
+                         mean=0.0,
+                         stddev=1.0,
+                         dtype=tf.dtypes.float32,
+                         seed=None,
+                         name='noise_batch_x')
+                    loss_gen = training_Generator(noise_batch_x)
                     tf.summary.scalar('loss_gen', loss_gen, step=gen_step)
                     mean_loss_gen += loss_gen
                 mean_loss_gen = mean_loss_gen/disc_steps_n
                 print('mean_loss_gen: {}'.format(mean_loss_gen))
-
+            
             # Save checkpoint after k epochs:
             disc_ckpt.step.assign_add(1)
             gen_ckpt.step.assign_add(1)
-            if epoch % 50 == 0:
+            if epoch % 100 == 99:
               disc_save_path = disc_ckpt_manager.save()
               gen_save_path = gen_ckpt_manager.save()
               print("Saved checkpoint for step {}: {}".format(int(disc_ckpt.step), disc_save_path))
               print("Saved checkpoint for step {}: {}".format(int(gen_ckpt.step), gen_save_path))
-              img_in_noise = tf.random.normal(shape = (1, noise_shape),
+            if epoch%1000 == 0:
+                normal_dist_input = tf.random.normal(shape = (100000, noise_shape),
                      mean=0.0,
                      stddev=1.0,
                      dtype=tf.dtypes.float32,
                      seed=None,
-                     name='img_in_noise')
-              img = tf.expand_dims(input = generator(img_in_noise), 
-                             axis = -1, name=None)
-              tf.summary.image("Generated data", img, step=epoch)
+                     name='noise_batch_x')
+                plt.subplot(2, 1, 1)
+                gen_dist = generator(normal_dist_input).numpy().flatten()
+                gen_dist = gen_dist[gen_dist <= 3.0]
+                _ = plt.hist(generator(normal_dist_input).numpy().flatten(), bins='auto')
+                plt.subplot(2, 1, 2)
+                pareto_dist = train_pareto[train_pareto <= 3.0]
+                _ = plt.hist(pareto_dist, bins='auto')
+                plt.show()
 
 
 # backend.clear_session()
