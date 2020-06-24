bdb9b5d2c8cf19a32e4ad2a4d53b57d78c47eb09
Matej Hladiš <matej.hladis@gmail.com>
2020-06-24 10:24:05 +0200

    See CoLab

---
+++ RL_Playground/RL_Playground/CoLab/run_in_Google_Colab.py
@@ -0,0 +1,5 @@
+
+
+# Thanks to PyDrive library GoogleColab can now be used form here.
+# See https://stackoverflow.com/questions/48376580/google-colab-how-to-read-data-from-my-google-drive
+# Change name of this file ! ! ! 
